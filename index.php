87366f4a0da909d478d6493d6899f7fcffb7b6e0
Nikita <nikita@web-dius.ru>
2020-05-06 15:29:39 +0300

    Test 06.05.2020 15:29

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $(window).ready(function(){
 	</head>
 	<body>
 <div id="basket_19">
-	<div id="basket_19_1">123123</div>
+	<div id="basket_19_1">222</div>
 	<div id="basket_19_2"></div>
 	<div id="basket_19_3"></div>
 	<div id="basket_19_4"></div>
