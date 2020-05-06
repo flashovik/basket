48c0e1a81082a6139ce5dccde3bbdb2aabf6a71d
Nikita <nikita@web-dius.ru>
2020-05-06 16:42:04 +0300

    Demo 5

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $(window).ready(function(){
 	</head>
 	<body>
 <div id="basket_19">
-	<div id="basket_19_1">777</div>
+	<div id="basket_19_1">888</div>
 	<div id="basket_19_2"></div>
 	<div id="basket_19_3"></div>
 	<div id="basket_19_4"></div>
